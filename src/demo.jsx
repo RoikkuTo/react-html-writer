3c40086615154a2f8867e4fea4813a832d58771e
Loïc <loictife@hotmail.fr>
2020-01-29 10:26:24 +0100

    Derniers correctifs

--- src/demo.jsx
+++ src/demo.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './App.css'
 import { HTMLTag, String } from './HTMLTag'
 
 const Demo = () => {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './index.css'
 import Demo from './demo.jsx'
 
 ReactDOM.render(<Demo />, document.getElementById('root'))
