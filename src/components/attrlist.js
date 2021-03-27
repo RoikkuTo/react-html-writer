1ef79f1bb354ebb088895109d04c4b40cb9292ba
RoikkuTo <loictife@hotmail.fr>
2021-03-27 16:19:23 +0100

    Fix AttrList component

--- src/components/attrlist.js
+++ src/components/attrlist.js
@@ -2,34 +2,34 @@ import React, { useEffect, useMemo, useRef, useState } from 'react'
 
 import styles from '../style/style.css'
 
-const Attr = ({ currGroup, cursor, idx, type, value }) => {
-	const prevCursor = useRef(null)
-	const str = useRef({
-		name: '',
-		value: '',
-		symbol: ''
-	})
+// const Attr = ({ currGroup, cursor, idx, type, value }) => {
+// 	const prevCursor = useRef(null)
+// 	const str = useRef({
+// 		name: '',
+// 		value: '',
+// 		symbol: ''
+// 	})
 
-	useEffect(() => {
-		if (currGroup === idx && cursor !== prevCursor.current) {
-			prevCursor.current = cursor
-			const prev = str.current
-			str.current = {
-				attr: prev.attr + (type === 'attr' ? value[cursor] : ''),
-				equal: prev.symbol + (type === 'equal' ? value[cursor] : ''),
-				value: prev.value + (type === 'value' ? value[cursor] : '')
-			}
-		}
-	}, [currGroup, cursor, idx])
+// 	useEffect(() => {
+// 		if (currGroup === idx && cursor !== prevCursor.current) {
+// 			prevCursor.current = cursor
+// 			const prev = str.current
+// 			str.current = {
+// 				attr: prev.attr + (type === 'attr' ? value[cursor] : ''),
+// 				equal: prev.symbol + (type === 'equal' ? value[cursor] : ''),
+// 				value: prev.value + (type === 'value' ? value[cursor] : '')
+// 			}
+// 		}
+// 	}, [currGroup, cursor, idx])
 
-	return (
-		<span className={`${styles.hwe} ${styles['tag-attr']}`}>
-			<span className={`${styles.hwe} ${styles['tag-attr__name']}`}>{str.current.name}</span>
-			<span className={`${styles.hwe} ${styles['tag-attr__symbol']}`}>{str.current.symbol}</span>
-			<span className={`${styles.hwe} ${styles['tag-attr__value']}`}>{str.current.value}</span>
-		</span>
-	)
-}
+// 	return (
+// 		<span className={`${styles.hwe} ${styles['tag-attr']}`}>
+// 			<span className={`${styles.hwe} ${styles['tag-attr__name']}`}>{str.current.name}</span>
+// 			<span className={`${styles.hwe} ${styles['tag-attr__symbol']}`}>{str.current.symbol}</span>
+// 			<span className={`${styles.hwe} ${styles['tag-attr__value']}`}>{str.current.value}</span>
+// 		</span>
+// 	)
+// }
 
 const AttrList = ({ pencil, data }) => {
 	// const [list, setList] = useState([])
@@ -45,44 +45,49 @@ const AttrList = ({ pencil, data }) => {
 	// const res = []
 	// data.forEach((group, i) => group.type === 'attr' && res.push(data.slice(i, i + 3)))
 	// <Attr key={Math.random()} currGroup={currGroup} cursor={cursor} idx={i + 2} {...group} />
-
-	const tab = useMemo(() => data.map((elm, i) => {
-		switch (elm.type) {
-			case 'attr': return {
-				...elm,
-				idx: i + 2,
-				str: '',
-				component(group, cursor) {
-					if (group === this.idx) this.str += this.value[cursor] || ''
-					return <span key={this.value + i} className={`${styles.hwe} ${styles['tag-attr__name']}`}>{this.str}</span>
-				}
-			}
-			case 'equal': return {
-				...elm,
-				idx: i + 2,
-				str: '',
-				component(group, cursor) {
-					if (group === this.idx) this.str += this.value[cursor] || ''
-					return <span key={this.value + i} className={`${styles.hwe} ${styles['tag-attr__symbol']}`}>{this.str}</span>
-				}
-			}
-			case 'value': return {
-				...elm,
-				idx: i + 2,
-				str: '',
-				component(group, cursor) {
-					if (group === this.idx) this.str += this.value[cursor] || ''
-					return <span key={this.value + i} className={`${styles.hwe} ${styles['tag-attr__value']}`}>{this.str}</span>
-				}
-			}
-		}
-	}), [])
+	const [attr, setAttr] = useState([])
+	const prevCursor = useRef(null)
 
 	const { group, cursor } = pencil
 
+	useEffect(() => {
+		setAttr(data.map((elm, i) => {
+			switch (elm.type) {
+				case 'attr': return {
+					...elm,
+					idx: i + 2,
+					str: '',
+					component(group, cursor) {
+						if (group === this.idx && cursor !== prevCursor.current) this.str += this.value[cursor] || ''
+						return <span key={this.value + i} className={`${styles.hwe} ${styles['tag-attr__name']}`}>{this.str}</span>
+					}
+				}
+				case 'equal': return {
+					...elm,
+					idx: i + 2,
+					str: '',
+					component(group, cursor) {
+						if (group === this.idx && cursor !== prevCursor.current) this.str += this.value[cursor] || ''
+						return <span key={this.value + i} className={`${styles.hwe} ${styles['tag-attr__symbol']}`}>{this.str}</span>
+					}
+				}
+				case 'value': return {
+					...elm,
+					idx: i + 2,
+					str: '',
+					component(group, cursor) {
+						if (group === this.idx && cursor !== prevCursor.current) this.str += this.value[cursor] || ''
+						return <span key={this.value + i} className={`${styles.hwe} ${styles['tag-attr__value']}`}>{this.str}</span>
+					}
+				}
+			}
+			prevCursor.current = cursor
+		}))
+	}, [data, setAttr])
+
 	return (
 		<span>
-			{tab.map(elm => elm.component(group, cursor))}
+			{attr.map(elm => elm.component(group, cursor))}
 		</span>
 	)
 }
